1182034041fd4f2f455022903a6ce78684522e81
Imran1910276131 <imran121767@gmail.com>
2023-11-02 21:11:49 +0600

    All works done and then committed

---
+++ VMDemo.java
@@ -0,0 +1,103 @@
+interface VMState {
+    void insert(int amount);
+    void select(String item);
+    void dispense(String item);
+}
+
+class Vending {
+    private VMState state;
+    private int balance;
+
+    public Vending() {
+        state = new NoMoney(this);
+    }
+
+    public void setState(VMState state) {
+        this.state = state;
+    }
+
+    public void insert(int amount) {
+        state.insert(amount);
+    }
+
+    public void select(String item) {
+        state.select(item);
+    }
+
+    public void dispense(String item) {
+        state.dispense(item);
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
+
+class NoMoney implements VMState {
+    private final Vending vm;
+
+    public NoMoney(Vending vm) {
+        this.vm = vm;
+    }
+
+    @Override
+    public void insert(int amount) {
+        vm.setBalance(amount);
+        System.out.println(amount + " inserted. Total balance: " + vm.getBalance());
+        vm.setState(new HasMoney(vm));
+    }
+
+    @Override
+    public void select(String item) {
+        System.out.println("Please insert money first.");
+    }
+
+    @Override
+    public void dispense(String item) {
+        System.out.println("Please insert money first.");
+    }
+}
+
+class HasMoney implements VMState {
+    private final Vending vm;
+
+    public HasMoney(Vending vm) {
+        this.vm = vm;
+    }
+
+    @Override
+    public void insert(int amount) {
+        vm.setBalance(vm.getBalance() + amount);
+        System.out.println(amount + " more inserted. Total balance: " + vm.getBalance());
+    }
+
+    @Override
+    public void select(String item) {
+        if (vm.getBalance() >= 5) {
+            vm.dispense(item);
+            vm.setBalance(vm.getBalance() - 5);
+        } else {
+            System.out.println("Insufficient balance. Please insert more money.");
+        }
+    }
+
+    @Override
+    public void dispense(String item) {
+        System.out.println("Dispensing " + item + ". Enjoy your " + item + "!");
+        vm.setState(new NoMoney(vm));
+    }
+}
+
+public class VMDemo {
+    public static void main(String[] args) {
+        Vending vm = new Vending();
+
+        vm.select("Soda"); 
+        vm.insert(10);     
+        vm.select("Chips");  
+    }
+}
